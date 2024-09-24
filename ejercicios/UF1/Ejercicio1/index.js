9fd0f10624b66d8acdafb1bdf91d606886617fcb
Luciaamo <156692681+Luciaamo@users.noreply.github.com>
2024-09-24 18:29:35 +0000

    "Modificación de archivos" 	new file:   ../.vscode/settings.json 	new file:   ../ejercicios/UF1/Ejercicio1/index.html 	new file:   ../ejercicios/UF1/Ejercicio1/index.js 	new file:   sesion1/index.css 	new file:   sesion1/index.html 	new file:   sesion1/index.js 	new file:   sesion2/index.html 	new file:   sesion2/index.js 	new file:   sesion2b/index.html 	new file:   sesion2b/index.js 	new file:   sesion3/index.html 	new file:   sesion3/index.js 	new file:   sesion4/index.html 	new file:   sesion4/index.js
    	modified:   ../.vscode/settings.json
    	modified:   sesion4/index.html
    	modified:   sesion4/index.js

---
+++ ejercicios/UF1/Ejercicio1/index.js
@@ -0,0 +1,56 @@
+console.log("Calculadora inicialitzada!");
+
+document.getElementById('btn1').addEventListener('click', function() {
+    console.log('1');
+});
+
+document.getElementById('btn2').addEventListener('click', function() {
+  console.log('2');
+});
+
+document.getElementById('btn1').addEventListener('click', function() {
+    document.getElementById('pantalla').textContent = '1';
+});
+
+document.getElementById('btn2').addEventListener('click', function() {
+  document.getElementById('pantalla').textContent = '2';
+});
+
+let entradaActual = '';
+
+document.getElementById('btn1').addEventListener('click', function() {
+  entradaActual += '1';
+  document.getElementById('pantalla').textContent = entradaActual;
+});
+
+document.getElementById('btnSuma').addEventListener('click', function() {
+    entradaActual += '+';
+    document.getElementById('pantalla').textContent = entradaActual;
+});
+
+document.getElementById('btnIgual').addEventListener('click', function() {
+    const resultat = eval(entradaActual);
+    console.log('Resultat:', resultat); // Testeja el resultat a la consola
+    document.getElementById('pantalla').textContent = resultat;
+});
+
+document.getElementById('btnBorrar').addEventListener('click', function() {
+    entradaActual = '';
+    document.getElementById('pantalla').textContent = '0';
+});
+
+document.getElementById('btnIgual').addEventListener('click', function() {
+    try {
+      if (entradaActual !== '') {
+        const resultat = eval(entradaActual);
+        document.getElementById('pantalla').textContent = resultat;
+      } else {
+        console.log('No hi ha res a calcular');
+      }
+    } catch (error) {
+      console.log('Error en l\'operació:', error);
+      document.getElementById('pantalla').textContent = 'Error';
+    }
+});
+  
+
---
+++ sesiones/sesion2/index.js
@@ -0,0 +1,29 @@
+console.log('Hola sesión 2');
+/* // SUMAR NÚMEROS
+// Pedir datos con prompt
+const numero = prompt('Introduce un número: ')
+console.log('numero: ', numero);
+const numero2 = prompt('Introduce otro número: ')
+console.log('numero2: ', numero2);
+console.log('suma: ', parseInt(numero)+parseInt(numero2)); */
+
+// JUEGO DE LOS NÚMEROS
+const numeroAdivinar = 10
+let intentos = 0
+let numeroIntroducido = -1
+while(intentos<3 && numeroAdivinar != numeroIntroducido){
+    console.log('intentos', intentos);
+    numeroIntroducido = prompt('Introduce un número: ')
+    console.log('numeroIntroducido: ' + numeroIntroducido);
+    
+    if(numeroIntroducido<numeroAdivinar){
+        console.log('El número es menor')
+    }else if(numeroIntroducido>numeroAdivinar){
+        console.log('El número es mayor')
+    }else{
+        console.log('El número es igual')
+    }
+
+    intentos++
+}
+
