4497570ff497afc335fdcd33bc5a179f8c7ca5cd
Luciaamo <156692681+Luciaamo@users.noreply.github.com>
2024-10-04 15:43:54 +0000

    index.html y index.js modificados

--- ejercicios/UF1/Ejercicio2/index.js
+++ ejercicios/UF1/Ejercicio2/index.js
@@ -1,51 +1,31 @@
-const randomNumber = Math.floor(Math.random() * 50); 
-console.log(randomNumber);
+console.log('hola');
 
-document.querySelector('#miElement').innerHTML = "<b>Hola món!</b>";
-
-const quiz = [
-    ["Quina és la capital de França?", "París", "Londres", "Berlín", "Madrid", 1],
-    ["Quina és la capital d'Espanya?", "Lisboa", "Madrid", "Roma", "Atenes", 2],
-    ["Quina és la capital d'Itàlia?", "Roma", "París", "Madrid", "Lisboa", 1]
+const preguntas = [
+  ["¿Cuál es la capital de Francia? Es una ciudad europea", "París", "Londres", "Berlín", "Madrid", 1],
+  ["¿Cuál es la capital de Espanya? Es una ciudad europea", "Lisboa", "Madrid", "Roma", "Atenes", 2],
+  ["¿Cuál es la capital de Italia? Es una ciudad europea", "Roma", "París", "Madrid", "Lisboa", 1]
 ];
 
-  
-  console.log(quiz[0][1]);
-  console.log(quiz[1][2]); 
-  console.log(quiz[2][1]);
-    
+document.querySelector('#question').innerHTML = preguntas[0][0]
 
-function saludar(nom) {
-    return "Hola, " + nom + "!";
-}
-console.log(saludar("Lucía"));  
+document.querySelector('#answer1').innerHTML = preguntas[0][1]
+document.querySelector('#answer2').innerHTML = preguntas[0][2]
+document.querySelector('#answer3').innerHTML = preguntas[0][3]
+document.querySelector('#answer4').innerHTML = preguntas[0][4]
 
-document.querySelector('#boton').addEventListener('click', function() {
-    alert("Has fet clic!");
-  });
 
-const resposta = 30;
-  if (temperatura > 25) {
-        console.log("Fa calor");
-  } else {
-        console.log("Fa fred");
-}
 
-const missatge = "";
-if (missatge === "") {
-  console.log("No has seleccionat cap resposta");
-} else {
-  console.log("Missatge vàlid.");
-}
 
-document.getElementById("result").style.display = "block";
 
-document.getElementById("result").style.display = "none";
 
-const resultDiv = document.getElementById("result");
 
-if (resultDiv.style.display === "none") {
-    resultDiv.style.display = "block";
-} else {
-    resultDiv.style.display = "none";
-}
+
+
+
+
+
+
+
+
+
+
